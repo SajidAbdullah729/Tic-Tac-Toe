51cb53cd09e59bd2f02f6371a1ad9e0abc09ec75
Mohammad Abdullah-Al-Sajid Chowdhury <75419355+SajidAbdullah729@users.noreply.github.com>
2025-03-22 08:27:56 +0600

    Update game.ts

--- game.ts
+++ game.ts
@@ -54,9 +54,9 @@ class TicTacToe {   // A class of Tic-Tac Toe which has multiple values
 
   checkWinner(): boolean {
     const winPatterns = [
-      [0, 1, 2], [3, 4, 5], [6, 7, 8],  // rows
-      [0, 3, 6], [1, 4, 7], [2, 5, 8],  // columns
-      [0, 4, 8], [2, 4, 6]              // diagonals
+      [0, 1, 2], [3, 4, 5], [6, 7, 8],  // rows win position
+      [0, 3, 6], [1, 4, 7], [2, 5, 8],  // columns win position
+      [0, 4, 8], [2, 4, 6]              // diagonals win position
     ];
     return winPatterns.some(pattern => {
       const [a, b, c] = pattern;
